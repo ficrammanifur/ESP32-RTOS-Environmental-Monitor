36f7538e842669e4ab4e06a0aac8160d638d6b40
Ficram Manifur Farissa <ficramm@gmail.com>
2026-01-13 16:58:32 +0700

    Create test_wifi.ino

---
+++ test/test_wifi/test_wifi.ino
@@ -0,0 +1,21 @@
+#include <WiFi.h>
+
+const char* ssid = "NAMA_WIFI";
+const char* password = "PASSWORD";
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+
+  Serial.print("Connecting");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("\nWiFi Connected!");
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {}
