cf6d0648e453bac23bf98fff25b20b7911dd5b5d
Ficram Manifur Farissa <ficramm@gmail.com>
2026-01-26 15:52:11 +0700

    Update test_ldr.ino

--- test/test_ldr/test_ldr.ino
+++ test/test_ldr/test_ldr.ino
@@ -1,12 +1,23 @@
-#define LDR_PIN 32
+#define LDR_PIN 34   // Pin LDR (ADC only)
 
 void setup() {
   Serial.begin(115200);
+  pinMode(LDR_PIN, INPUT);
 }
 
 void loop() {
-  int lightValue = analogRead(LDR_PIN);
-  Serial.print("LDR Value: ");
-  Serial.println(lightValue);
+  int ldrValue = analogRead(LDR_PIN);
+
+  // Interpretasi nilai LDR (sesuai kode asli kamu)
+  String ldrStatus = (ldrValue > 800) ? "GELAP" :
+                     (ldrValue < 450) ? "TERANG" :
+                                        "SEDANG";
+
+  Serial.println("========== LDR ==========");
+  Serial.print("LDR Value  : ");
+  Serial.println(ldrValue);
+  Serial.print("LDR Status : ");
+  Serial.println(ldrStatus);
+
   delay(1000);
 }
